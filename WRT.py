3c48ec37f8029181612d6e91b2e5f3393e73b7ce
PhilNad <philippe.nadeau1991@gmail.com>
2022-01-01 20:49:51 -0500

    Better testing

--- WRT.py
+++ WRT.py
@@ -248,12 +248,26 @@ class DbConnector:
             return GetSet(con)
 
 db = DbConnector()
-mat = np.eye(4)
-mat[0:3, 3] = [1,1,1]
-db.In('my-world').Set('table').Wrt('world').Ei('world').As(mat)
-pose = SE3.Rx(30, "deg", t=[0.5,0.5,0])
-db.In('my-world').Set('object').Wrt('table').Ei('table').As(pose)
+pose = np.array([[1,0,0,1],[0,1,0,1],[0,0,1,1],[0,0,0,1]])
+db.In('test').Set('a').Wrt('world').Ei('world').As(pose)
 
-db.In('my-world').Get('object').Wrt('world').Ei('world')
+pose = SE3.Rx(90, "deg")
+db.In('test').Set('b').Wrt('a').Ei('a').As(pose)
 
-Visualize('my-world')
+pose = SE3.Rx(0, "deg", t=[1,0,0])
+db.In('test').Set('c').Wrt('b').Ei('b').As(pose)
+
+pose = SE3.Rz(90, "deg", t=[1,1,0])
+db.In('test').Set('d').Wrt('b').Ei('b').As(pose)
+
+
+assert(db.In('test').Get('a').Wrt('b').Ei('b')          == SE3(np.array([[1,0,0,0],[0,0,1,0],[0,-1,0,0],[0,0,0,1]])))
+assert(db.In('test').Get('a').Wrt('b').Ei('a')          == SE3(np.eye(4)))
+assert(db.In('test').Get('c').Wrt('world').Ei('world')  == SE3(np.array([[1,0,0,2],[0,0,-1,1],[0,1,0,1],[0,0,0,1]])))
+assert(db.In('test').Get('c').Wrt('world').Ei('c')      == SE3(np.array([[1,0,0,2],[0,1,0,1],[0,0,1,-1],[0,0,0,1]])))
+assert(db.In('test').Get('c').Wrt('world').Ei('a')      == SE3(np.array([[1,0,0,2],[0,0,-1,1],[0,1,0,1],[0,0,0,1]])))
+assert(db.In('test').Get('d').Wrt('a').Ei('a')          == SE3(np.array([[0,-1,0,1],[0,0,-1,0],[1,0,0,1],[0,0,0,1]])))
+
+Visualize('test')
+
+
